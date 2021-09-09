fc0b6a09cd3326e03093f3644666f901dd36b615
monii <moniyel@gmail.com>
2021-09-09 21:26:10 +0900

    Chore css loader 방식을 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
-        filename: 'bundle.js'
-    },
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname),
-                exclude: /(node_modules)|(dist)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist']
-        }),
-        new HtmlWebpackPlugin({
-          template: "./index.html",
-        }),
-        new CopyPlugin({
-            patterns: [
-              { from: "./assets", to: "assets" },
-            ],
-          }),
-      ],
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./assets", to: "assets" }],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
